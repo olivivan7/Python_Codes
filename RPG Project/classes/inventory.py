abe012ad24188618227d97b64bfd12c9b5142e26
Ivan Madeira De Oliveira <67763471+olivivan7@users.noreply.github.com>
2022-07-19 12:49:24 -0300

    Delete RPG Project directory

--- RPG Project/classes/inventory.py
+++
@@ -1,9 +0,0 @@
-
-class itens:
-    def __init__(self,name,type,description,hp,mp,dmg):
-        self.name =  name
-        self.type = type
-        self.description = description
-        self.hp = hp
-        self.mp = mp
-        self.dmg = dmg
